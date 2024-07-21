6dc9d6d480cc7ebb6b06b670ba98b9e7f0e07dbc
Jacob M. Ramey <jacob.ramey@qrctech.com>
2024-07-21 14:36:11 -0400

    moved references to SDR to sdrfly module

--- backend/src/sdr_plot_backend/utils.py
+++ backend/src/sdr_plot_backend/utils.py
@@ -1,4 +1,5 @@
 import threading
+from sdrfly.sdr.sdr_generic import SDRGeneric
 
 class sdr_scheduler_config:
     
@@ -18,6 +19,24 @@ class sdr_scheduler_config:
         self.dc_suppress = True
         self.number_of_peaks = 5
         self.recordings_dir = "/root/workspace/data/recordings"
-        pass
+        
+
+        self.radio_name = "sidekiq"
+        self.hackrf_sdr =  SDRGeneric(self.radio_name, center_freq=self.center_freq, sample_rate=self.sample_rate, bandwidth=self.sample_rate, gain=self.gain, size=self.sample_size)
+        self.hackrf_sdr.start()
+    
+    def reselect_radio(self, name : str) -> int:
+        names = ["sidekiq", "hackrf"]
+        for _name in names:
+            if _name in name:
+                self.hackrf_sdr.stop()
+                self.hackrf_sdr.close()
+                self.hackrf_sdr = None
+                self.radio_name = name
+                self.hackrf_sdr =  SDRGeneric(self.radio_name, center_freq=self.center_freq, sample_rate=self.sample_rate, bandwidth=self.sample_rate, gain=self.gain, size=self.sample_size)
+                return 0
+        return 1
+        
+
 
 vars = sdr_scheduler_config()
